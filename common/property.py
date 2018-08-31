608ccb0df8e10830da5be184d2a5b88c6e75bcc2
qiushengming@aliyun.com <sunshine>
2018-08-31 10:44:57 +0800

    `ImportError: cannot import name`bug修复 原因：异常是因为两个py文件互相import导致的异常。悲催发现是因为在没注意到原先文件代码中已经存在import关系

--- common/property.py
+++ common/property.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
 # qiushengming-minnie
-from common.utils.ip_proxy_pool import GET_PROXY_IP
-
 USER_AGENT = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
